3976d7dccd360276156799bfb25e96cb8e425794
dude lao <brycelipscomb@gmail.com>
2022-12-06 22:38:17 -0500

    Keyboard component for Hangman along with a style module to house styles -- using the component rather than inline styles so we can take advantage of the :hover and :focus selectors

--- src/components/HangmanKeyboard.tsx
+++ src/components/HangmanKeyboard.tsx
@@ -1,5 +1,11 @@
+import KEYS from "../utility/keys.json";
+import styles from "./HangmanKeyboard.module.css";
+
 export default function HangmanKeyboard() {
-    return <div>
+    return <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))", gap: ".5rem" }} >
+        {KEYS.map(key => (
+            <button className={`${styles.btn}`} key={key}>{key}</button>
+        ))}
 
     </div>
 }
