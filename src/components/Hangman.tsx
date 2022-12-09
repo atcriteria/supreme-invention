b9bbd8c461a2657acef16555614da74cf5b920e3
dude lao <brycelipscomb@gmail.com>
2022-12-08 20:30:22 -0500

    completed the rest of the implementation for hangman -- still need to fix a bug where you can continue to input letters with the keyboard even after you've guess or won/lost

--- src/components/Hangman.tsx
+++ src/components/Hangman.tsx
@@ -1,19 +1,63 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import words from "../utility/wordlist.json";
 import HangmanDrawing from "./HangmanDrawing";
 import HangmanKeyboard from "./HangmanKeyboard";
 import HangmanWord from "./HangmanWord";
 
-const GetRandomWord = () => {
+const getRandomWord = () => {
     return words[Math.floor(Math.random() * words.length)]
 }
 
 export default function Hangman() {
-    const [wordToGuess, setWordToGuess] = useState(GetRandomWord());
+    const [wordToGuess, setWordToGuess] = useState(getRandomWord());
     const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
 
-    const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter))
+    const incorrectLetters = guessedLetters.filter(letter => !wordToGuess.includes(letter));
+
+    const isLoser = incorrectLetters.length >= 6; // 6 body parts
+    // .every iterates over the letters and returns true if the callback logic is the same
+    // ie, if every letter in the word is in the letters we have guessed, each call on the
+    // callback will be true thus the .every will return true, else false.
+    const isWinner = wordToGuess.split("").every(letter => guessedLetters.includes(letter))
+
+    const addGuessedLetter = useCallback((letter: string) => {
+        if (guessedLetters.includes(letter) || isLoser || isWinner) return;
+
+        setGuessedLetters(currentLetters => [...currentLetters, letter]);
+    }, [guessedLetters, isWinner, isLoser]);
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key;
+            if (!key.match(/^[a-z]$/)) return;
+            e.preventDefault();
+            addGuessedLetter(key);
+        }
+
+        document.addEventListener("keypress", handler);
+
+        return () => {
+            document.removeEventListener("keypress", handler);
+        }
+    }, [])
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key;
+            if (key !== "Enter") return;
+
+            e.preventDefault();
+            setGuessedLetters([]);
+            setWordToGuess(getRandomWord());
+        }
+
+        document.addEventListener("keypress", handler);
+
+        return () => {
+            document.removeEventListener("keypress", handler);
+        }
+    }, [])
 
     return (
         <div style={{
@@ -25,12 +69,17 @@ export default function Hangman() {
             alignItems: "center"
         }}>
             <div style={{ fontSize: "2rem", textAlign: "center" }}>
-                Lose / Win
+                {isWinner && "Winner! - Refresh to tru again"}
+                {isLoser && "Loser! - Refresh to tru again"}
             </div>
             <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
-            <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
+            <HangmanWord reveal={isLoser} guessedLetters={guessedLetters} wordToGuess={wordToGuess} />
             <div style={{ alignSelf: "stretch" }} >
-                <HangmanKeyboard />
+                <HangmanKeyboard activeLetters={guessedLetters.filter(letter => wordToGuess.includes(letter))}
+                    inactiveLetters={incorrectLetters}
+                    addGuessedLetter={addGuessedLetter}
+                    disabled={isWinner || isLoser}
+                />
             </div>
         </div>
     )
--- src/components/HangmanKeyboard.tsx
+++ src/components/HangmanKeyboard.tsx
@@ -1,11 +1,28 @@
 import KEYS from "../utility/keys.json";
 import styles from "./HangmanKeyboard.module.css";
 
-export default function HangmanKeyboard() {
+type KeyboardProps = {
+    activeLetters: string[],
+    inactiveLetters: string[],
+    addGuessedLetter: (letter: string) => void,
+    disabled?: boolean
+}
+
+export default function HangmanKeyboard({ activeLetters, inactiveLetters, addGuessedLetter, disabled = false }: KeyboardProps) {
     return <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))", gap: ".5rem" }} >
-        {KEYS.map(key => (
-            <button className={`${styles.btn}`} key={key}>{key}</button>
-        ))}
+        {KEYS.map(key => {
+            const isActive = activeLetters.includes(key);
+            const isInactive = inactiveLetters.includes(key);
+            return (
+                <button onClick={() => addGuessedLetter(key)} className={`${styles.btn} ${isActive ? styles.active : ""} 
+                ${isInactive ? styles.inactive : ""}`} key={key}
+                    disabled={isInactive || isActive || disabled}
+                >
+                    {key}
+                </button>)
+        }
+        )
+        }
 
     </div>
 }
